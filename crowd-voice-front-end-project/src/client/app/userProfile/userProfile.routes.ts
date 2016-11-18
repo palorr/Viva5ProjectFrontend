fe5d22ef78fc0e9932b325b6938501b5adda6f06
palorr <archontellis29@gmail.com>
2016-11-18 19:53:28 +0200

    ++

--- crowd-voice-front-end-project/src/client/app/userProfile/userProfile.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { userProfileComponent } from './index';
-
-export const userProfileRoutes: Route[] = [
-  	{
-    	path: 'userProfile',
-    	component: userProfileComponent
-  	}
-];
