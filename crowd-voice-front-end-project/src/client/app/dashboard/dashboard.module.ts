9c466a5df4d64f86022be6c3c7f12beafa214eca
Archontellis Rafael Sotirchellis <archontellis29@gmail.com>
2016-11-19 11:05:22 +0200

    started userProfile (archontellis)

--- crowd-voice-front-end-project/src/client/app/dashboard/dashboard.module.ts
+++ crowd-voice-front-end-project/src/client/app/dashboard/dashboard.module.ts
@@ -7,6 +7,8 @@ import { ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { ProjectModule } from './project/project.module';
 import { ProjectListModule } from './projectList/projectList.module';
 
+import { userProfileModule } from './userProfile/userProfile.module' ; 
+
 import { HomeModule } from './home/home.module';
 import { ChartModule } from './charts/chart.module';
 import { BlankPageModule } from './blank-page/blankPage.module';
@@ -37,7 +39,8 @@ import {SidebarComponent} from '../shared/index';
         GridModule,
     	BSComponentModule,
         BSElementModule,
-        BlankPageModule
+        BlankPageModule,
+        userProfileModule
     ],
     declarations: [DashboardComponent, TopNavComponent, SidebarComponent],
     exports: [DashboardComponent, TopNavComponent, SidebarComponent]
---
+++ crowd-voice-front-end-project/src/client/app/dashboard/userProfile/userProfile.routes.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+
+import { userProfileComponent } from './index';
+
+export const userProfileRoutes: Route[] = [
+  	{
+    	path: 'userProfile',
+    	component: userProfileComponent
+  	}
+];
