ce47e2b587613b8ec9e8bcd2e958059ef2dcfe72
palorr <archontellis29@gmail.com>
2016-11-24 20:36:47 +0200

    some fixes
    Now the guest cant have checks for notifications
    The Project Uptdates Tab is invisible until the first project update

--- crowd-voice-front-end-project/src/client/app/shared/topnav/topnav.ts
+++ crowd-voice-front-end-project/src/client/app/shared/topnav/topnav.ts
@@ -10,82 +10,84 @@ import { CurrentUser, Project } from '../../models/index';
 import { AuthenticationService, UserService } from '../../services/index';
 
 @Component({
-    moduleId: module.id,
-    selector: 'top-nav',
-    templateUrl: 'topnav.html',
+	moduleId: module.id,
+	selector: 'top-nav',
+	templateUrl: 'topnav.html',
 })
 
 export class TopNavComponent implements OnInit {
-	
+
 	projectsToNotify: Project[];
-	
+
 	isLoggedIn: boolean = false;
-	
-	currentUser = {}; 
-	
+
+	currentUser = {};
+
 	intervals: any[] = [];
-	
+
 	constructor(
 		private authGuard: AuthGuard,
 		private currentUserService: CurrentUserService,
 		private userService: UserService,
 		private authenticationService: AuthenticationService,
 		private route: ActivatedRoute,
-        private router: Router
-	){}
-	
+		private router: Router
+	) { }
+
 	ngOnInit() {
-		
-		if(this.authGuard.isUserLoggedIn()) {
+
+		if (this.authGuard.isUserLoggedIn()) {
 			this.isLoggedIn = true;
 		}
-		
-		if(localStorage.getItem('currentUser')) {
+
+		if (localStorage.getItem('currentUser')) {
 			this.currentUserService.getUserMainInfo()
-			.subscribe(
-                (data) => {
-                    this.currentUser = data;
+				.subscribe(
+				(data) => {
+					this.currentUser = data;
 					console.log('Current User: ', this.currentUser);
-                },
-                (err) => {
-                    console.log('ERROR: ', err);
-                });	
-		
+				},
+				(err) => {
+					console.log('ERROR: ', err);
+				});
+
 			//this.checkForNotifications();
-			
+
 		}
 	}
-	
+
 	logOut() {
 		this.authenticationService.logout();
 		this.router.navigate(['/login']);
 	}
-	
+
 	checkForNotifications() {
-		let self = this;
-		this.intervals.push(
-				window.setInterval(function() {
+		if (this.isLoggedIn) {
+			let self = this;
+			this.intervals.push(
+				window.setInterval(function () {
 					self.userService
 						.getUserFundedCompletedProjects(false)
 						.subscribe(
-							(data: Project[]) => {
-								self.projectsToNotify = data;
-								console.log('Completed Projects: ', self.projectsToNotify);
-							},
-							(err) => {
-								console.log('ERROR: ', err);
-							}
+						(data: Project[]) => {
+							self.projectsToNotify = data;
+							console.log('Completed Projects: ', self.projectsToNotify);
+						},
+						(err) => {
+							console.log('ERROR: ', err);
+						}
 						);
 				}, 10000)
 			);
+		}
 	}
-	
+
 	changeTheme(color: string): void {
 		var link: any = $('<link>');
 		link
 			.appendTo('head')
-			.attr({type : 'text/css', rel : 'stylesheet'})
-			.attr('href', 'themes/app-'+color+'.css');
+			.attr({ type: 'text/css', rel: 'stylesheet' })
+			.attr('href', 'themes/app-' + color + '.css');
 	}
 
 	rtl(): void {
@@ -93,7 +95,7 @@ export class TopNavComponent implements OnInit {
 		body.toggleClass('rtl');
 	}
 
-	sidebarToggler(): void  {
+	sidebarToggler(): void {
 		var sidebar: any = $('#sidebar');
 		var mainContainer: any = $('.main-container');
 		sidebar.toggleClass('sidebar-left-zero');
